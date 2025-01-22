4c6344c7dfae7a110dd5f038816ca12547661234
unknown <ayotunde1900@gmail.com>
2025-01-22 09:45:28 +0100

    first commit

--- app/setup/layout.jsx
+++ app/setup/layout.jsx
@@ -1,18 +1,14 @@
-'use client'
+"use client";
 import Image from "next/image";
 import React from "react";
-import { Bounce, ToastContainer } from 'react-toastify'
-import logo from '@/assets/images/logo.png'
-import { createContext } from "vm";
+import { Bounce, ToastContainer } from "react-toastify";
+import logo from "@/assets/images/logo.png";
 
-export const MessageContext = createContext()
 function Layout({ children }) {
-
-
   return (
-    <main className='fixed w-full h-full flex justify-center font-rubikRegular text-sm'>
+    <main className="fixed w-full h-full flex justify-center font-rubikRegular text-sm">
       <div className="w-10/12 max-w-[500px] min-h-[400px] m-auto">
-        <Image src={logo} className='w-[200px] m-auto' alt='Weave logo' />
+        <Image src={logo} className="w-[200px] m-auto" alt="Weave logo" />
         {children}
         <ToastContainer
           position="top-right"
