d6eb7dd827e3cb9e1f10f61a7672a44ae8b75691
owoeyeadenike2020 <112137934+owoeyeadenike2020@users.noreply.github.com>
2025-03-07 17:29:48 +0100

    Edit user update

--- components/usersList/userRender.jsx
+++ components/usersList/userRender.jsx
@@ -109,12 +109,14 @@ function UserRender({ info, date, resendInvite,onEditClick  }) {
 
           <div className="absolute right-0 rounded-md p-2 shadow bg-white text-xs w-[200px]  dropdown-menu">
             <div className="flex flex-col text-left">
-            <button
-                onClick={() => onEditClick(info)}
-                className="px-3 py-1 text-left hover:bg-gray-100"
-              >
-                <i className="fa fa-pencil mr-2"></i> Edit User
-              </button>
+            <div
+                    onClick={() => {
+                      onEditClick(info);
+                    }}
+                    className="px-3 py-1 text-left hover:bg-gray-100 cursor-pointer"
+                  >
+                    <i className="fa fa-pencil mr-2"></i> Edit User
+                  </div>
               <Link
                 href={`/users?modal=delete-admin&id=${info.id}`}
                 className="text-red-500 px-3 py-1"
